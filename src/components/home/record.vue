2b983410bb7a5672769cdcfdc43159fbe4063a3b
jiang <youyadexuanlv@qq.com>
2018-01-23 17:43:45 +0800

    首页完成

--- src/components/home/record.vue
+++ src/components/home/record.vue
@@ -1,16 +1,35 @@
 <template>
-<div>
+<div class="record-container">
   <jt-header :title="title"></jt-header>
-  <div>消费记录</div>
+  <!--消费记录列表-->
+  <mu-list>
+    <!--列表头容器-->
+    <mu-sub-header class="mu-list-header">
+      <!--子导航-->
+      <record-header :title="title"></record-header>
+    </mu-sub-header>
+    <!--列表项-->
+    <mu-list-item class="mu-list-item">
+      <div class="record-item">
+        <div class="record-item-left" slot="left">
+          <p class="record-date">2018/01/16 10:30</p>
+          <p class="record-type">刷卡消费</p>
+        </div>
+        <div class="record-item-right" slot="right">-200</div>
+      </div>
+    </mu-list-item>
+  </mu-list>
 </div>
 </template>
 
 <script>
 import jtHeader from '../jtHeader'
+import recordHeader from './recordHeader'
 export default {
   name: 'record',
   components: {
-    jtHeader
+    jtHeader,
+    recordHeader
   },
   data () {
     return {
@@ -20,6 +39,38 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @mixin jt-flex($dir, $wrap, $main, $cross) {
+    display: flex;
+    flex-flow: $dir $wrap;
+    justify-content: $main;
+    align-items: $cross;
+  }
+  /*消费记录容器*/
+  .record-container {
+    width: 100%;
+    height: 100vh;
+    @include jt-flex(column, nowrap, flex-start, center);
+    background-color: white;
+  }
+  /*列表头部*/
+  .mu-list-header {
+    width: 100%;
+    padding: 0;
+  }
+  /*列表项*/
+  .record-item {
+    padding: 5px;
+    @include jt-flex(row, nowrap, space-between, center)
+  }
+  /*列表项左侧容器*/
+  .record-item-left {
+    width: 40vw;
+    @include jt-flex(column, nowrap, center, center);
+    p {
+      margin: 0;
+      width: 40vw;
+    }
+  }
 
 </style>
